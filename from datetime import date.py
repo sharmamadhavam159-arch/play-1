975058238a787d7788a5cef0ea38e808b653642b
sharmamadhavam159-arch <sharmamadhavam159@gmail.com>
2025-10-27 20:19:50 +0530

    Add files via upload

---
+++ from datetime import date.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+# Store birthday dates in individual variables
+birthday1 = date(1980, 9, 12)  # Year, Month, Day
+birthday2 = date(1980, 7, 18)
+birthday3 = date(2010, 8,12 )
+birthday4 = date(1956, 7, 6)
+birthday5 = date(2006, 6, 14)
+
+# Print the stored birthdays to verify
+print(f"Birthday 1: {birthday1}")
+print(f"Birthday 2: {birthday2}")
+print(f"Birthday 3: {birthday3}")
+print(f"Birthday 4: {birthday4}")
+print(f"Birthday 5: {birthday5}")
