8c9a15d871b3f3bdd57d3c0bc87adfd0a7d4ac5f
sharmamadhavam159-arch <sharmamadhavam159@gmail.com>
2025-11-04 21:09:38 +0530

    Add files via upload

---
+++ activity 1 no 4.py
@@ -0,0 +1,6 @@
+print("enter the number to check whether number is positve or negiteve")
+x = int(input("enter the number"))
+if x >= 0 : 
+    print("positive numer ",x)
+else:
+    print("negitive number")
