ca86cff2455d29a40cbf752eebd5bb68841eb8ff
sharmamadhavam159-arch <sharmamadhavam159@gmail.com>
2025-11-22 15:55:25 +0530

    Add files via upload

---
+++ project no20.py
@@ -0,0 +1,4 @@
+base_num = float(input("Enter your base number"))
+exponent = float(input("Enter your exponent"))
+result = base_num ** exponent 
+print(f"{base_num} raised to the power {exponent} is:{result}")
