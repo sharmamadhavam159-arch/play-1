736eddeb866596d8cbb2a0809d619378c1bf27d9
sharmamadhavam159-arch <sharmamadhavam159@gmail.com>
2025-11-22 16:13:36 +0530

    Add files via upload

---
+++ project no 15.py
@@ -0,0 +1,6 @@
+age_str = input("please enter your age")
+age = int(age_str)
+if age >=10 and age <= 20 :
+    print(" This age is within the 10 to 20 range")
+else:
+    print("This age is outsidethe 10 to 20 range")
