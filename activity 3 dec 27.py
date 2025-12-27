be4b59effabc35f09c464fbcee3bc7f95a22b33a
sharmamadhavam159-arch <sharmamadhavam159@gmail.com>
2025-12-27 19:06:01 +0530

    Add files via upload

---
+++ activity 3 dec 27.py
@@ -0,0 +1,8 @@
+import math
+print("the floor and celling valueof 23,56 are:")
+str(math.ceil(23.56))+ ','+str(math.floor (23.56))
+x = 10 
+y = -15
+print("the value of x after copying the sign from y is: "+ str(math.copysign(x,y)))
+print("absulute value of -96 and 56 are:"+ str(math.fabs(-96))+','+str(math.fabs(56)))
+print("the gcd of 25 and 56 :"+ str(math.gcd(24, 54)))
